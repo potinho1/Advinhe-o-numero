724c311a207a2fef36fc3c409cb79648fb3bf3c8
João Vitor Gomes da Silva <gomesjoaovitor222@gmail.com>
2023-01-13 23:46:58 -0300

    Refatorando os prints

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import random
 
 jogadas = 0
 
-while True:
-    computador = random.randint(1, 11)
 
-    jogador = int(input("Teste acertar o número que o computador está pensando de 1 a 10\n"))
+while True:
+    jogador = int(input("""Tente acertar o número que o computador está pensando!
+    Números de 1 a 10\n"""))
+
+    computador = random.randint(1, 11)
 
     if computador == jogador:
         print("Acertou!")
@@ -13,6 +15,6 @@ while True:
     
     elif computador != jogador:
         jogadas += 1
-        print("Teste novamente\n")
+        print("Tente novamente...\n")
 
 print(f"Demorou {jogadas+1} rodadas para acertar!")
